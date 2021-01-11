993768ed8139b4dedf7f58aeb4b8e6748bbc34bf
Dancho Atanasov <dancho.d.atanasov@gmail.com>
2021-01-11 14:03:22 -0500

    fixed bug

--- src/library.cpp
+++ src/library.cpp
@@ -1,21 +1,23 @@
 // library.cpp
 
 #include "library.h"
+#include <iostream>
 
 int addOne(int number){
     return number + 1;
 }
 
 int farm(int (*worker)(int), int arr_len, int* input_arr) {
-    int * result = (int*)malloc(arr_len * sizeof(int));
+    //int * result = (int*)malloc(arr_len * sizeof(int));
+    int result[arr_len];
     for (int i = 0; i < arr_len; i++) {
         result[i] = (*worker)(input_arr[i]);
     }
     for (int i = 0; i < arr_len; i++) {
         printf("%d ", result[i]);
     }
-    prinf("\n");
-    free(result);
+    printf("\n");
+    //free(result);
 
     return 1;
 }
