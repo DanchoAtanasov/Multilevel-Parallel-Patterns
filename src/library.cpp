054d78ad6ad6940323745f111a8e7894750016a1
ddatanasov <datanasov@anomali.com>
2021-01-11 18:36:00 +0000

    Testing farm function prototype

--- src/library.cpp
+++ src/library.cpp
@@ -6,3 +6,7 @@ int addOne(int number){
     return number + 1;
 }
 
+int farm(int (*fcnPtr)(int)) {
+    return (*fcnPtr)(5);
+}
+
--- src/library.h
+++ src/library.h
@@ -2,3 +2,7 @@
 
 int addOne(int number);
 
+int farm(int (*fcnPtr)(int));
+
+//int (*fcnPtr)(int) { &foo }; // Initialize fcnPtr with function foo
+//(*fcnPtr)(5); // call function foo(5) through fcnPtr.
