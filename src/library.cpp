4384ab3956f6d90df86ecad6d6cc129d957696c2
ddatanasov <datanasov@anomali.com>
2021-01-12 10:27:49 +0000

    Adding .vs to .gitignore

--- src/library.cpp
+++ src/library.cpp
@@ -2,13 +2,15 @@
 
 #include "library.h"
 #include <iostream>
+#include <pthread.h>
 
 int addOne(int number){
     return number + 1;
 }
 
-int farm(int (*worker)(int), int arr_len, int* input_arr) {
+int farm(int (*worker)(int arg), int arr_len, int* input_arr) {
     //int * result = (int*)malloc(arr_len * sizeof(int));
+    printf("arg: %d\n", arg);
     int result[arr_len];
     for (int i = 0; i < arr_len; i++) {
         result[i] = (*worker)(input_arr[i]);
