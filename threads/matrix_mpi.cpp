ec963e47cb89b14a94c146fb29b5f3fdfb7f824b
ddatanasov <datanasov@anomali.com>
2021-01-28 15:37:45 +0000

    Testing MPI_Scatter

--- threads/matrix_mpi.cpp
+++ threads/matrix_mpi.cpp
@@ -1,40 +1,51 @@
 #include "mpi.h"
-   #include <stdio.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])  {
-   int numtasks, rank, next, prev, buf[2], tag1=1, tag2=2;
-   MPI_Request reqs[4];   // required variable for non-blocking calls
-   MPI_Status stats[4];   // required variable for Waitall routine
+#define MATRIX_SIZE 10
 
-   MPI_Init(&argc,&argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   
-   // determine left and right neighbors
-   prev = rank-1;
-   next = rank+1;
-   if (rank == 0)  prev = numtasks - 1;
-   if (rank == (numtasks - 1))  next = 0;
+float matrix1[MATRIX_SIZE][MATRIX_SIZE];
+float matrix2[MATRIX_SIZE][MATRIX_SIZE];
+float matrix3[MATRIX_SIZE][MATRIX_SIZE];
 
-   // post non-blocking receives and sends for neighbors
-   MPI_Irecv(&buf[0], 1, MPI_INT, prev, tag1, MPI_COMM_WORLD, &reqs[0]);
-   MPI_Irecv(&buf[1], 1, MPI_INT, next, tag2, MPI_COMM_WORLD, &reqs[1]);
+struct _data {
+    float m1[MATRIX_SIZE][MATRIX_SIZE];
+    float m2[MATRIX_SIZE][MATRIX_SIZE];
+} data;
 
-   MPI_Isend(&rank, 1, MPI_INT, prev, tag2, MPI_COMM_WORLD, &reqs[2]);
-   MPI_Isend(&rank, 1, MPI_INT, next, tag1, MPI_COMM_WORLD, &reqs[3]);
-  
-      // do some work while sends/receives progress in background
+int main(int argc, char* argv[]) {
+    int numtasks, rank, sendcount, recvcount, source;
+
+    for (int i = 0; i < MATRIX_SIZE; i++)
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            matrix1[i][j] = 2.0f;
+            matrix2[i][j] = 3.0f;
+        }
+
+    /*float sendbuf[SIZE][SIZE] = {
+       {1.0, 2.0},
+       {5.0, 6.0}
+    };*/
+    float sendbuf = matrix1;
+    float recvbuf[SIZE];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+    if (numtasks == SIZE) {
+        source = 1;
+        sendcount = SIZE;
+        recvcount = SIZE;
+        MPI_Scatter(sendbuf, sendcount, MPI_FLOAT, recvbuf, recvcount,
+            MPI_FLOAT, source, MPI_COMM_WORLD);
+
+        printf("rank= %d  Results: %f %f\n", rank, recvbuf[0],
+            recvbuf[1]);
+    }
+    else
+        printf("Must specify %d processors. Terminating.\n", SIZE);
+
+    MPI_Finalize();
+}
 
 
-   for(int i=0;i<1000000;i++){
-       if(i % 100000 == 0) printf("%d\n", i);
-   }
-
-   // wait for all non-blocking operations to complete
-   MPI_Waitall(4, reqs, stats);
-  
-      // continue - do more work
-
-   if(rank == 0) printf("Wait completed!\n");
-   MPI_Finalize();
-   }
