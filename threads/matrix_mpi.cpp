89e7b784917b147db4044c36e53addf347c56035
ddatanasov <datanasov@anomali.com>
2021-01-28 17:19:17 +0000

    Playing with MPI

--- threads/matrix_mpi.cpp
+++ threads/matrix_mpi.cpp
@@ -1,7 +1,7 @@
 #include "mpi.h"
 #include <stdio.h>
 
-#define MATRIX_SIZE 2
+#define MATRIX_SIZE 10
 
 float matrix1[MATRIX_SIZE][MATRIX_SIZE];
 float matrix2[MATRIX_SIZE][MATRIX_SIZE];
@@ -15,43 +15,67 @@ struct _data {
 int main(int argc, char* argv[]) {
     int numtasks, rank, sendcount, recvcount, source;
 
+    int numtasks, rank, tag1 = 1;
+
+    MPI_Request reqs[1];   // required variable for non-blocking calls
+    MPI_Status stats[1];   // required variable for Waitall routine
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     for (int i = 0; i < MATRIX_SIZE; i++)
         for (int j = 0; j < MATRIX_SIZE; j++) {
             matrix1[i][j] = 2.0f;
             matrix2[i][j] = 3.0f;
         }
 
-    /*float sendbuf[SIZE][SIZE] = {
-       {1.0, 2.0},
-       {5.0, 6.0}
-    };*/
-    //float sendbuf[][] = matrix1;
-    float recvbuf[2][MATRIX_SIZE];
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-    if (numtasks == MATRIX_SIZE) {
-        source = 1;
-        sendcount = MATRIX_SIZE;
-        recvcount = MATRIX_SIZE;
-        MPI_Scatter(matrix1, sendcount, MPI_FLOAT, recvbuf[0], recvcount,
-            MPI_FLOAT, source, MPI_COMM_WORLD);
-
-        printf("rank= %d  Results: %f %f\n", rank, recvbuf[0][0],
-            recvbuf[0][1]);
-        
-	MPI_Scatter(matrix2, sendcount, MPI_FLOAT, recvbuf[1], recvcount,
-            MPI_FLOAT, source, MPI_COMM_WORLD);
-        
-	printf("rank= %d  Results: %f %f\n", rank, recvbuf[1][0],
-            recvbuf[1][1]);
+   const int SPLIT = MATRIX_SIZE / numtasks;
+    int from = rank * SPLIT;
+    int to = from + SPLIT;
+    int res[MATRIX_SIZE * SPLIT];
+    for (int i = from; i < to; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            float c = 0.0f;
+            for (int k = 0; k < MATRIX_SIZE; k++) {
+                c += matrix1[i][k] * matrix2[k][j];
+            }
+            res[MATRIX_SIZE * i + j] = c;
+            //matrix3[i][j] = c;
+        }
     }
-    else
-        printf("Must specify %d processors. Terminating.\n", MATRIX_SIZE);
+
+    int buf[MATRIX_SIZE * SPLIT];
+
+    int payload = 313;
+    if (rank == 0) {
+        printf("Greetings from rank %d\n", rank);
+        printf("res: %d, %d\n", res[0], res[MATRIX_SIZE * MATRIX_SIZE / numtasks - 1]);
+        MPI_Isend(res, MATRIX_SIZE * SPLIT, MPI_INT, 1, tag1, MPI_COMM_WORLD, &reqs[0]);
+    }
+    else {
+        printf("Greetings from rank %d\n", rank);
+        printf("res: %d, %d\n", res[0], res[MATRIX_SIZE * MATRIX_SIZE / numtasks - 1]);
+        MPI_Irecv(buf, MATRIX_SIZE * SPLIT, MPI_INT, 0, tag1, MPI_COMM_WORLD, &reqs[0]);
+    }
+
+    
+
+    MPI_Waitall(1, reqs, stats);
+
+    if (rank == 0) {
+        printf("Greetings from rank %d again\n", rank);
+        //    printf("Integer in received buffer is %d\n", buf[0]);
+    }
+    else {
+        printf("Greetings from rank %d again\n", rank);
+        //printf("Integer in received buffer is %d\n", buf[0]);
+        printf("Integer in received buffer is %d, %d\n", buf[0], buf[MATRIX_SIZE * SPLIT - 1]);
+    }
 
     MPI_Finalize();
 }
+
+
 
 
