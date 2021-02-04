3925dc50b868d62b30a8ce6067e19fae42a402f2
ddatanasov <datanasov@anomali.com>
2021-02-04 17:02:39 +0000

    Testing possible bug, code refactoring

--- threads/matrix_mpi.cpp
+++ threads/matrix_mpi.cpp
@@ -9,15 +9,15 @@ float matrix3[MATRIX_SIZE][MATRIX_SIZE];
 
 const int TAG = 1;
 
-void work(float batch_matrix1[][MATRIX_SIZE], int batch_size, float matrix_result[][MATRIX_SIZE]) {
+void work(float submatrix[][MATRIX_SIZE], int SUBMATRIX_ROWS, float result_matrix[][MATRIX_SIZE]) {
     printf("Working beep boop...\n");
-    for (int i = 0; i < batch_size; i++) {
+    for (int i = 0; i < SUBMATRIX_ROWS; i++) {
        for (int j = 0; j < MATRIX_SIZE; j++) {
             float c = 0.0f;
             for (int k = 0; k < MATRIX_SIZE; k++) {
-                c += batch_matrix1[i][k] * matrix2[k][j];
+                c += submatrix[i][k] * matrix2[k][j];
             }
-            matrix_result[i][j] = c;
+            result_matrix[i][j] = c;
         }
     }
 }
@@ -26,21 +26,21 @@ void work(float batch_matrix1[][MATRIX_SIZE], int batch_size, float matrix_resul
 int main(int argc, char* argv[]) {
     int numtasks, rank;
     
-    MPI_Request reqs[5];
-    MPI_Status stats[5];
-
     // Initialize MPI
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    const int BATCH_SIZE = MATRIX_SIZE / numtasks;
-    float batch_matrix1[BATCH_SIZE][MATRIX_SIZE];
-    float matrix_result[BATCH_SIZE][MATRIX_SIZE];
+    MPI_Request reqs[5];
+    MPI_Status stats[5];
+
+    const int MATRIX_TOTAL_SIZE = MATRIX_SIZE * MATRIX_SIZE;
+    const int SUBMATRIX_ROWS = MATRIX_SIZE / numtasks;
+    const int SUBMATRIX_TOTAL_SIZE = SUBMATRIX_ROWS * MATRIX_SIZE;
+    float submatrix[SUBMATRIX_ROWS][MATRIX_SIZE];
+    
     
     if (rank == 0) {
-        printf("Greetings from rank %d\n", rank);
-
         // Initialize matrix values
         for (int i = 0; i < MATRIX_SIZE; i++)
             for (int j = 0; j < MATRIX_SIZE; j++) {
@@ -48,66 +48,52 @@ int main(int argc, char* argv[]) {
                 matrix2[i][j] = 3.0f;
             }
 
+        // Send matrix values to all nodes including itself
         for (int dest = 0; dest < numtasks; dest++) {
-            // Send matrix values
-            MPI_Isend(matrix1+(dest * BATCH_SIZE), BATCH_SIZE * MATRIX_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[0]);
-            MPI_Isend(matrix2, MATRIX_SIZE * MATRIX_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[1]);
+            MPI_Isend(matrix1 + (dest * SUBMATRIX_ROWS), SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[0]);
+            MPI_Isend(matrix2, MATRIX_TOTAL_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[1]);
         }
     }
-    //else {
-        printf("Greetings from rank %d\n", rank);
 
-        //float _matrix1[BATCH_SIZE][MATRIX_SIZE];
-        //float _matrix2[MATRIX_SIZE][MATRIX_SIZE];
+    printf("Greetings from rank %d\n", rank);
 
-        // Receive matrices
-        //MPI_Irecv(dancho, 2, MPI_FLOAT, 0, tag1, MPI_COMM_WORLD, &reqs[0]);
-        MPI_Irecv(batch_matrix1, BATCH_SIZE * MATRIX_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[0]);
-        MPI_Irecv(matrix2, MATRIX_SIZE * MATRIX_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[1]);
-        
-    //}
+    // Receive matrices
+    MPI_Irecv(submatrix, SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[0]);
+    MPI_Irecv(matrix2, MATRIX_TOTAL_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[1]);
 
     MPI_Waitall(2, reqs, stats);
 
-    float matrix_res[BATCH_SIZE][MATRIX_SIZE];
+    float final_matrix[SUBMATRIX_ROWS][MATRIX_SIZE];
     if (rank == 0) {
-
-	for(int dest=0;dest<numtasks;dest++){
-            MPI_Irecv(matrix_res, BATCH_SIZE * MATRIX_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[2]);
-	}
-        //printf("Greetings from rank %d again\n", rank);
-
-        //work(batch_matrix1, BATCH_SIZE, matrix_result);
-        //printf("matrix_result[0][0] after work is:  %.2f\n", matrix_result[0][0]);
-        //MPI_Irecv(matrix_res, BATCH_SIZE * MATRIX_SIZE, MPI_FLOAT, 1, TAG, MPI_COMM_WORLD, &reqs[2]);
+        // Receive results from other nodes
+        for (int dest = 0; dest < numtasks; dest++) {
+            MPI_Irecv(final_matrix + (dest * SUBMATRIX_TOTAL_SIZE), SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, dest, TAG, MPI_COMM_WORLD, &reqs[2]);
+        }
     }
-    //else {
-        printf("Greetings from rank %d again\n", rank);
-        printf("batch_matrix1[0][0] is:  %.2f\n", batch_matrix1[0][0]);
-        printf("matrix1[0][0] is:  %.2f\n", matrix1[0][0]);
-        printf("matrix2[0][0] is:  %.2f\n", matrix2[0][0]);
-        work(batch_matrix1, BATCH_SIZE, matrix_result);
-        
-        printf("matrix_result[0][0] after work is:  %.2f\n", matrix_result[0][0]);
 
-        MPI_Isend(matrix_result, BATCH_SIZE * MATRIX_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[2]);
-    //}
+    float result_matrix[SUBMATRIX_ROWS][MATRIX_SIZE];
+
+    printf("Greetings from rank %d again\n", rank);
+    printf("submatrix[0][0] is:  %.2f\n", submatrix[0][0]);
+    printf("matrix1[0][0] is:  %.2f\n", matrix1[0][0]);
+    printf("matrix2[0][0] is:  %.2f\n", matrix2[0][0]);
+    work(submatrix, SUBMATRIX_ROWS, result_matrix);
+        
+    printf("result_matrix[0][0] after work is:  %.2f\n", result_matrix[0][0]);
+
+    // Send result of work back to main node
+    MPI_Isend(result_matrix, SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, 0, TAG, MPI_COMM_WORLD, &reqs[2]);
 
     MPI_Wait(&reqs[2], &stats[2]);
 
     if (rank == 0) {
         printf("Greetings from rank %d for the third time\n", rank);
-        printf("Received matrix_res[0][0] is %.2f\n", matrix_res[0][0]);
+        printf("Received result_matrix[0][0] is %.2f\n", final_matrix[0][0]);
 
+        // Combine results and print matrix
         for(int i = 0; i < MATRIX_SIZE; i++){
 	        for(int j = 0; j < MATRIX_SIZE; j++){
-                    matrix3[i][j] = matrix_res[i - rank*BATCH_SIZE][j];
-                /*if(i < BATCH_SIZE){
-		            matrix3[i][j] = matrix_result[i][j];
-		        }
-		        else{
-		            matrix3[i][j] = matrix_res[i-BATCH_SIZE][j];
-		        }*/
+                    matrix3[i][j] = final_matrix[i - rank * SUBMATRIX_ROWS][j];
 	            printf("%.2f ", matrix3[i][j]); 
 	        }
 	        printf("\n");
