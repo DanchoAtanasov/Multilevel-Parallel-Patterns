6c61517736de1ad6586031fea4cc70ba6a8bdea0
ddatanasov <datanasov@anomali.com>
2021-02-16 16:13:23 +0000

    Adding matrix_mpi code to library_mpi.h

--- src/library_mpi.h
+++ src/library_mpi.h
@@ -2,12 +2,11 @@
 #ifndef _LIBRARY_MPI_H_
 #define _LIBRARY_MPI_H_
 
-//#include <iostream>
 #include "mpi.h"
 #include <stdio.h>
 
 template<typename R, typename... Args>
-int farm(R(*worker)(Args...), float m1[][100], float m2[][100], float m3[][100]){ 
+int farm(R(*worker)(Args...), const int MATRIX_SIZE, float m1[][100], float m2[][100], float m3[][100]){ 
     printf("In farm\n");
 	int numtasks, rank, sendcount, recvcount, source;
 
@@ -18,24 +17,57 @@ int farm(R(*worker)(Args...), float m1[][100], float m2[][100], float m3[][100])
 	MPI_Request reqs[3];
 	MPI_Status stats[3];
 
-	int payload;
+	const int MATRIX_TOTAL_SIZE = MATRIX_SIZE * MATRIX_SIZE;
+	const int SUBMATRIX_ROWS = MATRIX_SIZE / numtasks;
+	const int SUBMATRIX_TOTAL_SIZE = SUBMATRIX_ROWS * MATRIX_SIZE;
+	float submatrix[SUBMATRIX_ROWS][MATRIX_SIZE];
 
 	if (rank == 0) {
-		payload = 42;
+		for (int i = 0; i < MATRIX_SIZE; i++)
+			for (int j = 0; j < MATRIX_SIZE; j++) {
+				matrix1[i][j] = 2.0f;
+				matrix2[i][j] = 3.0f;
+			}
 	}
 
 	source = 0;
-	sendcount = 1;
-	recvcount = 1;
+	sendcount = SUBMATRIX_TOTAL_SIZE;
+	recvcount = SUBMATRIX_TOTAL_SIZE;
 
-	// Broadcasting the whole matrix2 to all nodes
-	MPI_Ibcast(&payload, 1, MPI_INT, source, MPI_COMM_WORLD, &reqs[0]);
+    // Scattering matrix1 to all nodes in chunks
+    MPI_Iscatter(matrix1, sendcount, MPI_FLOAT, submatrix, recvcount,
+        MPI_FLOAT, source, MPI_COMM_WORLD, &reqs[0]);
+    // Broadcasting the whole matrix2 to all nodes
+    MPI_Ibcast(matrix2, MATRIX_SIZE * MATRIX_SIZE, MPI_FLOAT,
+        source, MPI_COMM_WORLD, &reqs[1]);
 
-	// Wait for messages to be received
-	MPI_Waitall(1, reqs, stats);
+    // Wait for messages to be received
+    MPI_Waitall(2, reqs, stats);
 
-	printf("sup %d\n", payload);
-	int res = (*worker)();
+    printf("rank: %d, submatrix[0][0]: %.2f\n", rank, submatrix[0][0]);
+    printf("rank: %d, matrix2[0][0]: %.2f\n", rank, matrix2[0][0]);
+
+    float result_matrix[SUBMATRIX_ROWS][MATRIX_SIZE];
+    // Call worker function, save result in result_matrix
+    int res = (*work)(submatrix, SUBMATRIX_ROWS, result_matrix);
+
+    // Gather results from all nodes to main node
+    MPI_Igather(result_matrix, sendcount, MPI_FLOAT, matrix3, recvcount,
+        MPI_FLOAT, source, MPI_COMM_WORLD, &reqs[2]);
+
+    if (rank == 0) {
+        MPI_Wait(&reqs[2], &stats[2]);
+
+        printf("Gathered results:\n");
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                printf("%.2f ", matrix3[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    printf("rank: %d finished exectuion.\n", rank);
 
 	MPI_Finalize();
 
--- threads/matmult_new_mpi.cpp
+++ threads/matmult_new_mpi.cpp
@@ -22,6 +22,20 @@ int matrixmult(int start, int end, double additionalOption) {
     return 1;
 }
 
+// Worker function that calculates matrix multiplication
+void work(float submatrix[][MATRIX_SIZE], int SUBMATRIX_ROWS, float result_matrix[][MATRIX_SIZE]) {
+    printf("Working beep boop...\n");
+    for (int i = 0; i < SUBMATRIX_ROWS; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            float c = 0.0f;
+            for (int k = 0; k < MATRIX_SIZE; k++) {
+                c += submatrix[i][k] * matrix2[k][j];
+                result_matrix[i][j] = c;
+            }
+        }
+    }
+}
+
 int foo() {
     printf("In Foo!\n");
     return 1;
@@ -37,7 +51,7 @@ int main() {
     //int res = farm(3, 100, matrixmult, 35.50);
 
     //int res = farm(foo, matrix1, matrix2, matrix3);
-    int res = farm(foo, matrix1, matrix2, matrix3) ;
+    int res = farm(work, MATRIX_SIZE, matrix1, matrix2, matrix3);
 
     printf("First and last values in the matrix are: %.0f, %.0f\n", matrix3[0][0], matrix3[MATRIX_SIZE - 1][MATRIX_SIZE - 1]);
 }
