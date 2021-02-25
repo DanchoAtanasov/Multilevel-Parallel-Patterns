cf8eb04d8475fc0ec74385778db30fb2d636796e
ddatanasov <datanasov@anomali.com>
2021-02-25 13:02:16 +0000

    Removing comments, preparing for merging the two libraries

--- src/library_mpi.h
+++ src/library_mpi.h
@@ -32,8 +32,6 @@ void scatter(float matrix[][10], int SIZE, float submatrix[][10]) {
     // Scattering matrix1 to all nodes in chunks
     MPI_Scatter(matrix, SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, submatrix, SUBMATRIX_TOTAL_SIZE,
         MPI_FLOAT, 0, MPI_COMM_WORLD);
-    /*MPI_Iscatter(matrix, SUBMATRIX_TOTAL_SIZE, MPI_FLOAT, submatrix, SUBMATRIX_TOTAL_SIZE,
-        MPI_FLOAT, 0, MPI_COMM_WORLD, &reqs[0]);*/
 }
 
 void broadcast(float matrix[][10], int SIZE) {
@@ -41,8 +39,6 @@ void broadcast(float matrix[][10], int SIZE) {
     // Broadcasting the whole matrix2 to all nodes
     MPI_Bcast(matrix, SIZE, MPI_FLOAT,
         0, MPI_COMM_WORLD);
-    /*MPI_Ibcast(matrix, SIZE, MPI_FLOAT,
-        0, MPI_COMM_WORLD, &reqs[1]);*/
 }
 
 void finish() {
@@ -55,44 +51,8 @@ void finish() {
 template<typename R, typename... Args>
 int farm(R(*worker)(Args...), const int MATRIX_SIZE, float submatrix[][10], float matrix3[][10]){
     printf("In farm\n");
-	/*int numtasks, rank, sendcount, recvcount, source;
-
-	MPI_Init(NULL, NULL);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-	MPI_Request reqs[3];
-	MPI_Status stats[3];*/
-
-	/*const int MATRIX_TOTAL_SIZE = MATRIX_SIZE * MATRIX_SIZE;
-	const int SUBMATRIX_ROWS = MATRIX_SIZE / numtasks;
-	const int SUBMATRIX_TOTAL_SIZE = SUBMATRIX_ROWS * MATRIX_SIZE;*/
-	//float submatrix[SUBMATRIX_ROWS][10];
-
-	/*if (rank == 0) {
-		for (int i = 0; i < MATRIX_SIZE; i++)
-			for (int j = 0; j < MATRIX_SIZE; j++) {
-				matrix1[i][j] = 2.0f;
-				matrix2[i][j] = 3.0f;
-			}
-	}*/
-
-	/*source = 0;
-	sendcount = SUBMATRIX_TOTAL_SIZE;
-	recvcount = SUBMATRIX_TOTAL_SIZE;*/
-
-    // Scattering matrix1 to all nodes in chunks
-    /*MPI_Iscatter(matrix1, sendcount, MPI_FLOAT, submatrix, recvcount,
-       MPI_FLOAT, source, MPI_COMM_WORLD, &reqs[0]);*/
-    //// Broadcasting the whole matrix2 to all nodes
-    //MPI_Ibcast(matrix2, MATRIX_SIZE * MATRIX_SIZE, MPI_FLOAT,
-    //    source, MPI_COMM_WORLD, &reqs[1]);
-
-    // Wait for messages to be received
-    //MPI_Waitall(2, reqs, stats);
 
     printf("rank: %d, submatrix[0][0]: %.2f\n", rank, submatrix[0][0]);
-    //printf("rank: %d, matrix2[0][0]: %.2f\n", rank, matrix2[0][0]);
 
     const int SUBMATRIX_TOTAL_SIZE = MATRIX_SIZE / numtasks;
     float result_matrix[SUBMATRIX_TOTAL_SIZE / 10][10];
