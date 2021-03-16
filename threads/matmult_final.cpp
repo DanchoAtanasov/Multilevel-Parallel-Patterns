546431ac9d5c45ef435ed0de229c9fd22db08abf
ddatanasov <datanasov@anomali.com>
2021-03-16 11:47:54 +0000

    Restoring to functioning version of matrix multiplication

--- threads/matmult_final.cpp
+++ threads/matmult_final.cpp
@@ -63,32 +63,32 @@ int main() {
 
     Init();
     
-    Load(init_d);
+    /*Load(init_d);
     printf("%d %d %d %d\n", d[0].a, d[0].b, d[1].a, d[1].b);
     myA.x = 5;
     myA.y = 7;
     doSomething(&myA, &A::x, &A::y);
     printf("after doSomething: %d, %d\n", myA.x, myA.y);
     Broadcast(d, 2);
-    printf("%d %d %d %d\n", d[0].a, d[0].b, d[1].a, d[1].b);
+    printf("%d %d %d %d\n", d[0].a, d[0].b, d[1].a, d[1].b);*/
     
-    //Load(init_matrices);
-    //Scatter(matrix1, MATRIX_SIZE * MATRIX_SIZE, submatrix);
+    Load(init_matrices);
+    Scatter(matrix1, MATRIX_SIZE * MATRIX_SIZE, submatrix);
     /*for(int i=0;i<4;i++)
         for(int j=0;j<8;j++)
             printf("sm[%d][%d]:%.2f\n", i, j, submatrix[i][j]);*/
-    //Broadcast(matrix2, MATRIX_SIZE * MATRIX_SIZE);
-    //int res = Farm(2, MATRIX_SIZE, matrixmult, 35.50);
-    //Gather(result, MATRIX_SIZE * MATRIX_SIZE, matrix3);
+    Broadcast(matrix2, MATRIX_SIZE * MATRIX_SIZE);
+    int res = Farm(2, MATRIX_SIZE, matrixmult, 35.50);
+    Gather(result, MATRIX_SIZE * MATRIX_SIZE, matrix3);
     Finish();
 
-    //printf("First and last values in the matrix are: %.0f, %.0f\n", matrix3[0][0], matrix3[MATRIX_SIZE - 1][MATRIX_SIZE - 1]);
-    /*for (int i = 0; i < MATRIX_SIZE; i++) {
+    printf("First and last values in the matrix are: %.0f, %.0f\n", matrix3[0][0], matrix3[MATRIX_SIZE - 1][MATRIX_SIZE - 1]);
+    for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
             printf("%.2f ", matrix3[i][j]);
         }
         printf("\n");
-    }*/
+    }
 
     return 0;
 }
