60a518fd57da1fc0122aa0bdfe4d37667f81d114
ddatanasov <datanasov@anomali.com>
2021-03-18 11:49:52 +0000

    Trying potential fix

--- threads/Blackscholes/blackscholes_final.cpp
+++ threads/Blackscholes/blackscholes_final.cpp
@@ -292,7 +292,7 @@ int main(int argc, char** argv)
     data = (OptionData*)malloc(SPLIT * sizeof(OptionData));
 
     // Make new MPI_Datatype
-    doSomething(&filedata[0], &OptionData::s, &OptionData::strike, &OptionData::r,
+    doSomething(&data[0], &OptionData::s, &OptionData::strike, &OptionData::r,
         &OptionData::divq, &OptionData::v, &OptionData::t, &OptionData::OptionType,
         &OptionData::divs, &OptionData::DGrefval);
     /*
