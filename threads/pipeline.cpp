4fd5d5b9cc0ebcf6c1249cad1b2abc2f5049b61d
ddatanasov <datanasov@anomali.com>
2021-03-26 13:24:50 +0000

    Push main changes

--- threads/pipeline.cpp
+++ threads/pipeline.cpp
@@ -34,24 +34,25 @@ int stage3(float a) {
 	return int(a) + 4;
 }
 
-void readImage(char* name) {
+int readImage(char* name) {
 	printf("Mock reading image with name %s\n", name);
 	for (int i = 0; i < IMAGE_SIZE; i++) {
 		for (int j = 0; j < IMAGE_SIZE; j++) {
 			image[i][j] = i * IMAGE_SIZE + j;
 		}
 	}
-	//return 1
+	return 1
 }
 
-void processImage() {
+int processImage() {
 	printf("Processing image\n");
 	printf("beep boop\n");
 	for (int i = 0; i < IMAGE_SIZE; i++) {
 		for (int j = 0; j < IMAGE_SIZE; j++) {
-			stg[i][j] += 5;
+			stg1[i][j] += 5;
 		}
 	}
+	return 1;
 }
 
 
